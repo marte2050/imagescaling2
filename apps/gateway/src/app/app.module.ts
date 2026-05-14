15d23bb13776761fe305f814c5cadcb01c558707
Jonathan Willian de Oliveira <jonathan.oliveira@ifsuldeminas.edu.br>
2026-05-14 13:17:24 -0300

    refactor(app): remove unused AppController and AppService from AppModule

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ImagescalingModule } from 'src/upload/imagescaling.module';
 
 @Module({
   imports: [ImagescalingModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
