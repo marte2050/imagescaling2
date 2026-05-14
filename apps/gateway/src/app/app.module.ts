883911a8172c680b102ca29f5c7ca73f5cdf916a
Jonathan Willian de Oliveira <jonathan.oliveira@ifsuldeminas.edu.br>
2026-05-14 12:53:23 -0300

    feat(imagescaling): refactor imagescaling module and controller, move to upload directory

--- apps/gateway/src/app/app.module.ts
+++ apps/gateway/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ImagescalingModule } from 'src/imagescaling/imagescaling.module';
+import { ImagescalingModule } from 'src/upload/imagescaling.module';
 
 @Module({
   imports: [ImagescalingModule],
