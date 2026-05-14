8c38901d45f737999a345a2efae7e19f4d37913b
Jonathan Willian de Oliveira <jonathan.oliveira@ifsuldeminas.edu.br>
2026-05-14 15:49:44 -0300

    refactor(main.ts): simplify microservice creation and remove unused logger

--- apps/imagescaling/src/main.ts
+++ apps/imagescaling/src/main.ts
@@ -1,18 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { KafkaOptions } from '@nestjs/microservices';
 import { kafkaMicroserviceOptions } from './kafka/kafka.module';
 
 async function bootstrap() {
-  const app = await NestFactory.createMicroservice<KafkaOptions>(
-    AppModule,
-    kafkaMicroserviceOptions,
-  );
+  const app = await NestFactory.createMicroservice<KafkaOptions>(AppModule, kafkaMicroserviceOptions);
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   await app.listen();
 }
-
-Logger.log('Image Scaling Service is running...');
 
 void bootstrap();
